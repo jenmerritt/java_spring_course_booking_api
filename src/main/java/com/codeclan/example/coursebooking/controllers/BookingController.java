f72bd76e20fa626ff4884700b60c636b33f2f50e
Jen Merritt <jenjomez@gmail.com>
2020-02-27 11:48:19 +0000

    controllers setup

--- src/main/java/com/codeclan/example/coursebooking/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/coursebooking/controllers/BookingController.java
@@ -1,4 +1,22 @@
 package com.codeclan.example.coursebooking.controllers;
 
+import com.codeclan.example.coursebooking.repositories.BookingRepository.BookingRepository;
+import com.codeclan.example.coursebooking.repositories.CourseRepository.CourseRepository;
+import com.codeclan.example.coursebooking.repositories.CustomerRepository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/bookings")
 public class BookingController {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
 }
